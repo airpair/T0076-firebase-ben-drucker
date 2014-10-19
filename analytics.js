39b7e39cf75db5a8b716f89133762fc3b9b85a9d
Shane Cole <shanecole@MacShane-2.local>
2014-10-19 16:24:37 -0600

    Initial commit

---
+++ analytics.js
@@ -0,0 +1,28 @@
+(function (Firebase) {
+  var analytics = new Firebase('https://airpair-analytics-tutorial.firebaseio.com/');
+  var activeVisitors = analytics.child('activeVisitors');
+
+  var visitor = {
+    path: window.location.pathname,
+    arrivedAt: Firebase.ServerValue.TIMESTAMP,
+    userAgent: navigator.userAgent
+  };
+
+  var activeVisitorRef = activeVisitors.push(visitor, function () {
+    activeVisitors.child(visitorId).once('value', function (snapshot) {
+      visitor.arrivedAt = snapshot.val().arrivedAt;
+      var pastVisitors = analytics.child('pastVisitors');
+      visitor.leftAt = Firebase.ServerValue.TIMESTAMP;
+      pastVisitors.child(visitorId).onDisconnect().set(visitor);
+      activeVisitorRef.onDisconnect().remove();
+    });
+  });
+
+  var visitorId = activeVisitorRef.name();
+
+  var totalVisitors = analytics.child('totalVisitors');
+  totalVisitors.once('value', function (snapshot) {
+    totalVisitors.set(snapshot.val() + 1);
+  });
+
+})(Firebase);
